375114232148589c61030f1b1a673f7133f3f0f9
Yingting Huang <huangyingting@outlook.com>
2021-03-05 07:52:18 +0800

    Add dependency for hub VPN

--- virtualwan/virtualwan.tf
+++ virtualwan/virtualwan.tf
@@ -16,18 +16,6 @@ resource "azurerm_virtual_hub" "virtualhub_1" {
   virtual_wan_id      = azurerm_virtual_wan.virtualwan.id
 }
 
-resource "azurerm_vpn_gateway" "vpng_1" {
-  name                = "vHub${var.geoid_1}GW"
-  resource_group_name = azurerm_resource_group.virtualwan.name
-  location            = var.location_1
-  virtual_hub_id      = azurerm_virtual_hub.virtualhub_1.id
-  bgp_settings {
-    asn         = 65515
-    peer_weight = 100
-  }
-  scale_unit = 1
-}
-
 resource "azurerm_virtual_hub" "virtualhub_2" {
   name                = "vHub${var.geoid_2}"
   resource_group_name = azurerm_resource_group.virtualwan.name
@@ -46,6 +34,20 @@ resource "azurerm_virtual_hub_connection" "virtualhub_1_conn_2" {
   name                      = "vHub${var.geoid_1}_Conn_2"
   virtual_hub_id            = azurerm_virtual_hub.virtualhub_1.id
   remote_virtual_network_id = azurerm_virtual_network.spoke2.id
+}
+
+resource "azurerm_vpn_gateway" "vpng_1" {
+  name                = "vHub${var.geoid_1}GW"
+  resource_group_name = azurerm_resource_group.virtualwan.name
+  location            = var.location_1
+  virtual_hub_id      = azurerm_virtual_hub.virtualhub_1.id
+  bgp_settings {
+    asn         = 65515
+    peer_weight = 100
+  }
+  scale_unit = 1
+
+  depends_on = [ azurerm_virtual_hub_connection.virtualhub_1_conn_1, azurerm_virtual_hub_connection.virtualhub_1_conn_2 ]
 }
 
 resource "azurerm_virtual_hub_connection" "virtualhub_2_conn_1" {
